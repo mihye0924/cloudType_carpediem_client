a68590d1ea78f0a56347f0e914145849c2d9a7fe
chomihye <mihye0924@naver.com>
2024-01-17 15:52:44 +0900

    2024.01.17 client profile test!

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -191,7 +191,7 @@ const AccountPage = () => {
                         <img src={
                           `${item.account_profile}` === 'profile-dummy.svg' ? 
                           `/assets/images/${item.account_profile}` : 
-                          `${import.meta.env.VITE_BACK_URL}/uploads/profile/${item.account_profile}`
+                          `${import.meta.env.VITE_BACK_URL}/app/public/uploads/profile/${item.account_profile}`
                           } alt="프로필 이미지"/> 
                       </Box>
                       <span>
@@ -267,7 +267,7 @@ const AccountPage = () => {
             <img src={
               !imgIs ?
                "/assets/images/profile-dummy.svg": 
-              `${import.meta.env.VITE_BACK_URL}/uploads/profile/${imgSavePath}`} alt="profile"
+              `${import.meta.env.VITE_BACK_URL}/app/public//uploads/profile/${imgSavePath}`} alt="profile"
               /> 
           </Box>   
           {
